4848386fa73e5a01d886454ab98bb36a3cd56aba
Mingyu <mingyusu9841@gmail.com>
2019-09-29 17:21:18 +1000

    Create the animation to add more realisms to player's movement.

--- Assets/Scripts/WalkingCamera.cs
+++ Assets/Scripts/WalkingCamera.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
+﻿/*  This module is used to model the walking camera navigation matephor, where
+    the camera will tilt left and right to add more realism to the player's
+    movement.
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WalkingCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Animation animation;
+    private bool isMoving;
+
+    private bool left;
+    private bool right;
+
     void Start()
     {
-        
+        left = true;
+        right = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
         
+        if (xMovement != 0 || zMovement != 0) {
+            isMoving = true;
+        }
+        
+        if (xMovement == 0 && zMovement == 0) {
+            isMoving = false;
+        }
+
+        WalkingAnimation();
+
+    }
+
+    void WalkingAnimation() {
+        if (isMoving == true) {
+            if (left == true) {
+                if (!animation.isPlaying) {
+                    animation.Play("walkLeft");
+                    left = false;
+                    right = true;
+                }
+            }
+            if (right == true) {
+                if (!animation.isPlaying) {
+                    animation.Play("walkRight");
+                    left = true;
+                    right = false;
+                }
+            }
+        }
     }
 }
