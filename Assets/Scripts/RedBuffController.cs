8aceed81732fb7eaa00a6594c457fb7ebdbcd395
Xiande Wen <visiande1998@gmail.com>
2019-10-27 13:06:25 +1100

    Add redbuff

---
+++ Assets/Scripts/RedBuffController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBuffController : MonoBehaviour {
+    
+    public float spinSpeed;
+        	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localRotation *= Quaternion.AngleAxis(Time.deltaTime * spinSpeed, Vector3.up);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			HealthManager hm = col.gameObject.GetComponent<HealthManager>();
+			hm.SetCurrHealth(hm.GetCurrHealth() + 20);
+
+			Destroy(gameObject);
+		}
+	}
+}
