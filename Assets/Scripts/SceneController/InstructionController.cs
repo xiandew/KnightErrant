79589dd4e34610baffc25b156a8eaa0c7d375b43
Mingyu <mingyusu9841@gmail.com>
2019-10-27 13:37:28 +1100

    Finish implementing the tutorial.

--- Assets/Scripts/SceneController/InstructionController.cs
+++ Assets/Scripts/SceneController/InstructionController.cs
@@ -8,6 +8,7 @@ public class InstructionController : MonoBehaviour
 
     public Text gameInstruction;
     public GameObject tutorialEndPrompt;
+    public SwarmManager npcManager;
 
     private int currentInstr;
     private ArrayList instructions;
@@ -16,14 +17,16 @@ public class InstructionController : MonoBehaviour
     void Start()
     {
         tutorialEndPrompt.SetActive(false);
+        npcManager.gameObject.SetActive(false);
 
         instructions = new ArrayList();
-        instructions.Add("Welcome to the world of Knight Errant");
+        instructions.Add("Welcome to the world of Knight Errant\nPress ENTER to continue");
         instructions.Add("Press W and S to move forward and backward");
         instructions.Add("Press A and D to move left and right");
         instructions.Add("Moving your mouse to look around");
         instructions.Add("Press SPACE to jump");
         instructions.Add("Click left mouse button to fire");
+        instructions.Add("Now, let's try to fight with an enemy");
         currentInstr = 0;
 
         gameInstruction.text = instructions[currentInstr].ToString();
@@ -36,18 +39,58 @@ public class InstructionController : MonoBehaviour
     }
 
     void HandleInstructions() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            currentInstr++;
-
-            if (currentInstr < instructions.Count) {
-                gameInstruction.text = instructions[currentInstr].ToString();
-            } 
-            else {
-                gameInstruction.gameObject.SetActive(false);
-                tutorialEndPrompt.SetActive(true);
-                Cursor.lockState = CursorLockMode.None;
-            }
+        switch(currentInstr) {
+            case 0:
+                if (Input.GetKeyDown(KeyCode.Return)) {
+                    currentInstr++;
+                }
+                break;
+            case 1:
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) {
+                    currentInstr++;
+                }   
+                break;
+            case 2:
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) {
+                    currentInstr++;
+                }   
+                break;
+            case 3:
+                float mouseX = Input.GetAxis("Mouse X");
+                float mouseY = Input.GetAxis("Mouse Y");
+                    if (mouseX != 0 || mouseY != 0) {
+                        currentInstr++;
+                    }
+                break;
+            case 4:
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    currentInstr++;
+                }   
+                break;
+            case 5:
+                if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                    npcManager.gameObject.SetActive(true);
+                    currentInstr++;
+                }
+                break;
+            case 6:
+                if (npcManager.swarmDestroyed) {
+                    currentInstr++;
+                }
+                break;
+            default:
+                break;
         }
+
+        if (currentInstr < instructions.Count) {
+            gameInstruction.text = instructions[currentInstr].ToString();
+        }
+        else {
+            Cursor.lockState = CursorLockMode.None;
+            tutorialEndPrompt.SetActive(true);
+            gameInstruction.gameObject.SetActive(false);
+        }
+
     }
 
     public void StartNewGame() {
